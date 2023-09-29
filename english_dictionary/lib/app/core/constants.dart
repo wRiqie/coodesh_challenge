72ce64e943e951e3c265b44aa083cf842f39dbc9
wRiqie <henriquegame512@outlook.com>
2023-09-29 19:01:54 -0300

    fix: adding missing constants

--- english_dictionary/lib/app/core/constants.dart
+++ english_dictionary/lib/app/core/constants.dart
@@ -2,4 +2,7 @@ class Constants {
   Constants._();
 
   static const baseUrl = 'https://api.dictionaryapi.dev/api';
+  static const defaultError =
+      'An unexpected error has occurred, please try again...';
+  static const actualSession = 'actualSession';
 }
